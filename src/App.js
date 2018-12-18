f3e390b0e97ea9ed5294a9aaa2e25ace180e5323
Mike Bell <mib00038@gmail.com>
2018-12-18 16:12:15 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import countries from './data/countries.json';
+
+const sortedCountries = countries.sort();
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            matchedCountries : [],
+            isCountrySelected : false,
+            countrySelected: ''
+        };
+    }
+
+    handleOnChange(event){
+        if (!sortedCountries.includes(event.target.value)){
+            this.setState({
+                isCountrySelected: false
+            })
+        }
+        this.setState({
+            countrySelected: event.target.value,
+            matchedCountries: sortedCountries.filter((country)=>country.toLowerCase().startsWith(event.target.value.toLowerCase()))
+        });
+    }
+
+    handleSelectCountry(country){
+        console.log("handleSelectCountry", country);
+        this.setState({
+            isCountrySelected: true,
+            countrySelected: country
+        })
+    }
+
+    dropDownCountriesList(){
+        return(
+            <div className="country-list">
+                {this.state.matchedCountries.map((country, index) => {
+                    return(
+                        <input key={index}
+                               type="text"
+                               className="dropdown-item"
+                               value={country}
+                               readOnly="readOnly"
+                               onClick={this.handleSelectCountry.bind(this, country)}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
+    render() {
+        const searchBoxStyle = this.state.isCountrySelected ? {
+            border: "2px solid limegreen"
+        } : {};
+        return (
+            <div className="App">
+
+                <header className="App-header">
+                    <input
+                        style={searchBoxStyle}
+                        className="search-box"
+                        type="search"
+                        name="CountrySearch"
+                        placeholder="Search..."
+                        value={this.state.countrySelected}
+                        onChange={this.handleOnChange.bind(this)}
+                        autoFocus
+                    />
+
+                    <div className="country-list-container">
+                        { !this.state.isCountrySelected && this.state.countrySelected.length > 0 && this.state.matchedCountries && this.dropDownCountriesList() }
+                    </div>
+                </header>
+
+            </div>
+        );
+    }
 }
 
 export default App;
